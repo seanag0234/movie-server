afac06a9e965ec901a3046c83e73f6ada6166212
Sean_George <seangeorge0@gmail.com>
2018-02-26 20:53:24 -0700

    Remove annoying log statements

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,16 +4,13 @@ const config = require('../config');
 const secret = config.getSecret();
 
 let checkUser = function (req, res, next) {
-    let authorization = req.headers.authorization;
-    console.log(authorization);
     if (
         req.headers
         && req.headers.authorization
         && req.headers.authorization.split(' ')[0] === 'JWT'
     ) {
-        let token = req.headers.authorization.split(' ')[1];
-        console.log(token.trim());
-        jwt.verify(token.trim(), secret, function (err, decode) {
+        let token = req.headers.authorization.split(' ')[1].trim();
+        jwt.verify(token, secret, function (err, decode) {
             if (err) return res.status(401).send();
             req.user = decode;
             next();
